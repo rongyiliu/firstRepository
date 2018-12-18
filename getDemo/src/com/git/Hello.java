dba6cbe4636b43620c147c5dc7847cef406c2dbb
rongyiliu <1843417581@qq.com>
2018-12-18 16:29:22 +0800

    添加了一个添加方法

--- getDemo/src/com/git/Hello.java
+++ getDemo/src/com/git/Hello.java
@@ -2,6 +2,11 @@ package com.git;
 
 public class Hello {
 	
+	public void add()
+	{
+		System.out.println("这是添加方法");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("这是我的第一个Git");
