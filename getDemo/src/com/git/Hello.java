3f04e2cb7c7638c3b7a75af2bab11e3744732804
rongyiliu <1843417581@qq.com>
2018-12-18 16:31:26 +0800

    添加了一个撩妹方法

--- getDemo/src/com/git/Hello.java
+++ getDemo/src/com/git/Hello.java
@@ -7,6 +7,11 @@ public class Hello {
 		System.out.println("这是添加方法");
 	}
 	
+	public void getMeiMei()
+	{
+		System.out.println("撩妹");
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("这是我的第一个Git");
